4c4ad412ae380fb614442707b42c12556b1cac88
Tim Spinks <tim@monkishtypist.com>
2019-09-19 08:36:04 -0700

    config updates for router base

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <img src="/logo.png" alt="RateRabbit - Home Loans, Direct Lender" class="logo">
+  <img src="~static/logo.png" alt="RateRabbit - Home Loans, Direct Lender" class="logo">
 </template>
 
 <style>
