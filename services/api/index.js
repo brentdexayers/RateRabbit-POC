c22fc31715d12abdfb1d235bc8f35abd244db427
Tim Spinks <tim@monkishtypist.com>
2020-04-13 21:00:12 -0700

    dev testing

--- services/api/index.js
+++ services/api/index.js
@@ -18,12 +18,24 @@ const authPayload = {
 
 export const authenticate = async () => {
   const { data } = await axios.post(`${apiUrl}/authenticate`, authPayload, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: authenticate', error)
+    })
   return data
 }
 
 export const getCreditRating = async (auth) => {
   axiosConfig.headers.Authorization = 'Bearer ' + auth.JWT
   const { data } = await axios.get(`${apiUrl}/creditrating`, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: getCreditRating', error)
+    })
   // console.log('Credit Rating Options', data)
   return data
 }
@@ -31,6 +43,12 @@ export const getCreditRating = async (auth) => {
 export const getLoanDocType = async (auth) => {
   axiosConfig.headers.Authorization = 'Bearer ' + auth.JWT
   const { data } = await axios.get(`${apiUrl}/loandoctype`, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: getLoanDocType', error)
+    })
   // console.log('Loan Doc Type Options', data)
   return data
 }
@@ -38,6 +56,12 @@ export const getLoanDocType = async (auth) => {
 export const getLoanProgram = async (auth) => {
   axiosConfig.headers.Authorization = 'Bearer ' + auth.JWT
   const { data } = await axios.get(`${apiUrl}/loanprogram`, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: getLoanProgram', error)
+    })
   // console.log('Loan Program Options', data)
   return data
 }
@@ -45,6 +69,12 @@ export const getLoanProgram = async (auth) => {
 export const getLoanPurpose = async (auth) => {
   axiosConfig.headers.Authorization = 'Bearer ' + auth.JWT
   const { data } = await axios.get(`${apiUrl}/loanpurpose`, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: getLoanPurpose', error)
+    })
   // console.log('Loan Purpose Options', data)
   return data
 }
@@ -52,6 +82,12 @@ export const getLoanPurpose = async (auth) => {
 export const getLoanRefinanceType = async (auth) => {
   axiosConfig.headers.Authorization = 'Bearer ' + auth.JWT
   const { data } = await axios.get(`${apiUrl}/loanrefinancetype`, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: getLoanRefinanceType', error)
+    })
   // console.log('Loan Refinance Type Options', data)
   return data
 }
@@ -59,6 +95,12 @@ export const getLoanRefinanceType = async (auth) => {
 export const getMaritalStatus = async (auth) => {
   axiosConfig.headers.Authorization = 'Bearer ' + auth.JWT
   const { data } = await axios.get(`${apiUrl}/status/statustypename/Marital%20Status`, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: getMaritalStatus', error)
+    })
   // console.log('Marital Status Options', data)
   return data
 }
@@ -66,6 +108,12 @@ export const getMaritalStatus = async (auth) => {
 export const getPropertyType = async (auth) => {
   axiosConfig.headers.Authorization = 'Bearer ' + auth.JWT
   const { data } = await axios.get(`${apiUrl}/propertytype`, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: getPropertyType', error)
+    })
   // console.log('Property Type Options', data)
   return data
 }
@@ -73,6 +121,12 @@ export const getPropertyType = async (auth) => {
 export const getPropertyUse = async (auth) => {
   axiosConfig.headers.Authorization = 'Bearer ' + auth.JWT
   const { data } = await axios.get(`${apiUrl}/propertyuse`, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: getPropertyUse', error)
+    })
   // console.log('Property Use Options', data)
   return data
 }
@@ -80,6 +134,12 @@ export const getPropertyUse = async (auth) => {
 export const getState = async (auth) => {
   axiosConfig.headers.Authorization = 'Bearer ' + auth.JWT
   const { data } = await axios.get(`${apiUrl}/state`, axiosConfig)
+    .then((res) => {
+      return res
+    })
+    .catch((error) => {
+      console.error('Error :: getState', error)
+    })
   // console.log('State Options', data)
   return data.filter((state) => {
     return state.active
